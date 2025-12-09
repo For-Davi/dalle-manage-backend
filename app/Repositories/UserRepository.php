fc28ccb9ab98741e54bb7d5ad11fce6fbe7c6cff
Luiz Carlos <luizcarlosdev000@gmail.com>
2025-12-09 13:43:53 -0300

    fix/consertado erro em remoção de acesso ao sistema para o funcionário

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -133,7 +133,7 @@ class UserRepository
         DB::table('employees')->where('user_id', $userId)->update(['user_id' => null, 'has_login_access' => 0]);
     }
 
-    public function delete($id, $deleteEmployee)
+    public function delete($id, $deleteEmployee = null)
     {
         $user = $this->findById($id);
         if ($user) {
